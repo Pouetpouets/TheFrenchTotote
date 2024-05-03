d99a86abfb70b883c3e06d9b90b7f6a382efb3e5
Lucas Legrand <legrand.lucas0@gmail.com>
2024-05-04 00:35:47 +0200

    fonts

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
-import Image from "next/image";
+
+import './fonts.css'
+
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-<div className="bg-blue text-white p-4"> Blue Background</div>
-<div className="bg-blueLight text-white p-4"> Blue Light</div>
+<div className="bg-blue text-white p-4 custom-text custom-text" > Blue Background</div>
+<div className="bg-blueLight text-white p-4 custom-text2"> Blue Light</div>
 <div className="bg-red text-white p-4"> Red</div>
 <div className="bg-RedLight text-white p-4"> Red</div>
 <div className="bg-brown text-white p-4">Fig1 Background</div>
