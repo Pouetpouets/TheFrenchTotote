512e3820cfcd5e68770756bd757da80b985d908d
Lucas Legrand <legrand.lucas0@gmail.com>
2024-05-04 01:16:07 +0200

    wip

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,23 @@
-
-import './fonts.css'
-import France from '../../public/asssets/frenchTot/France.png'
-
+import './fonts.css';
+import France from '../../public/asssets/frenchTot/France.png';
 
 export default function Home() {
   return (
-    <main className="p-0">
-<p>
-The french tototte
-</p>
-<img src={France} />
-<p>
-pour ceux qui osent être différents
-</p>
+    <main className='p-0'>
+      <p>The french tototte</p>
 
-<div>
-  <div></div>
-</div>
 
+      <img src={France} />
+      <p>pour ceux qui osent être différents</p>
+
+      <div>
+        <div></div>
+
+test
+test2
+
+      </div>
+    <p>test3</p>
     </main>
   );
 }
