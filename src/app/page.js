8f451a4a21ed4bd63bff254f9cb249af68d65c69
Lucas Legrand <legrand.lucas0@gmail.com>
2024-05-04 00:54:48 +0200

    wip

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,23 @@
 
 import './fonts.css'
-
+import France from '../../public/asssets/frenchTot/France.png'
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-<div className="bg-blue text-white p-4 custom-text custom-text" > Blue Background</div>
-<div className="bg-blueLight text-white p-4 custom-text2"> Blue Light</div>
-<div className="bg-red text-white p-4"> Red</div>
-<div className="bg-RedLight text-white p-4"> Red</div>
-<div className="bg-brown text-white p-4">Fig1 Background</div>
-<div className="bg-white text-black p-4">BG1 Background</div>
+    <main className="p-0">
+<p>
+The french tototte
+</p>
+<img src={France} />
+<p>
+pour ceux qui osent être différents
+</p>
+
+<div>
+  <div></div>
+</div>
+
     </main>
   );
 }
